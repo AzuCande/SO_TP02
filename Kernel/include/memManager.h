440a96e4aebbb87e9f128cd2e72a4438381b3966
Angie <aborracci@itba.edu.ar>
2021-10-25 10:08:20 -0300

    Divided memManager .c and .h

--- Kernel/include/memManager.h
+++ Kernel/include/memManager.h
@@ -1,4 +1,28 @@
 #ifndef MEM_MANAGER_H
 #define MEM_MANAGER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdint.h>
+
+#define TOTAL_HEAP_SIZE (1024*1024)
+#define MIN_BLOCK_SIZE 100
+#define BYTE_ALIGNMENT 8
+#define BYTE_ALIGNMENT_MASK 0x0007
+#define ADJUSTED_HEAP_SIZE	( TOTAL_HEAP_SIZE - BYTE_ALIGNMENT )
+#define POINTER_SIZE_TYPE uint32_t
+
+typedef struct freeMemList_t {
+    struct freeMemList_t * nextFreeBlock;
+    size_t freeBlockSize;
+}freeMemList_t;
+
+typedef freeMemList_t * freeMemList_p;
+
+void initBlocksMM();
+void *mallocMemory(size_t sizeRequired);
+void freeMemory(void * free);
+void insertFreeBlockOnList(freeMemList_p block);
+
 #endif
--- Kernel/memManager.c
+++ Kernel/memManager.c
@@ -1,29 +1,7 @@
 // To compile: gcc -std=c99 -pedantic -Wall memManager.c -o memManager
 // To execute: ./memManager
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <stdint.h>
-
-#define TOTAL_HEAP_SIZE (1024*1024)
-#define MIN_BLOCK_SIZE 100
-#define BYTE_ALIGNMENT 8
-#define BYTE_ALIGNMENT_MASK 0x0007
-#define ADJUSTED_HEAP_SIZE	( TOTAL_HEAP_SIZE - BYTE_ALIGNMENT )
-#define POINTER_SIZE_TYPE uint32_t
-
-typedef struct freeMemList_t {
-    struct freeMemList_t * nextFreeBlock;
-    size_t freeBlockSize;
-}freeMemList_t;
-
-typedef freeMemList_t * freeMemList_p;
-
-void initBlocksMM();
-void *mallocMemory(size_t sizeRequired);
-void freeMemory(void * free);
-void insertFreeBlockOnList(freeMemList_p block);
+#include "include/memManager.h"
 
 uint8_t heap[TOTAL_HEAP_SIZE];
 
@@ -158,12 +136,3 @@ void freeMemory(void * free){
         insertFreeBlockOnList(toAddFreeBlock);
     }
 }
-
-// int main(){
-
-
-//     void * m1 = mallocMemory(100);
-
-//     freeMemory(m1);
-//     return 0;
-// }
