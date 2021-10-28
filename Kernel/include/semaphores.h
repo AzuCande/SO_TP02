1e3e1bf5de51f61e337963afd6766ba923540989
Juan <jdeluca@itba.edu.ar>
2021-10-28 19:27:58 -0300

    wait and open semaphore

--- Kernel/include/semaphores.h
+++ Kernel/include/semaphores.h
@@ -3,13 +3,16 @@
 
 #include <stdint.h>
 #include <memManager.h>
+#include <lib.h>
+#include <stddef.h>
 
 #define MAX_BLOCKED_PID 20
 
 typedef struct semType {
     uint32_t id;
-    uint32_t value; // state
+    uint32_t value; // state ?
     uint32_t blockedPIDs[MAX_BLOCKED_PID];
+    int mutex;
     struct semType * next;
 } semType;
 
@@ -17,6 +20,7 @@ typedef struct semList {
     uint64_t semQty;
     semType * first;
     semType * last;
+    semType * iterator;
 } semList;
 
 #endif
--- Kernel/sync/semaphores.c
+++ Kernel/sync/semaphores.c
@@ -2,7 +2,27 @@
 
 semList * semaphoresList = NULL;
 
+int initSemaphores() {
+    semaphoresList = mallocMemory(sizeof(semList));
+
+    if(semaphoresList == NULL) {
+        return -1;
+    }
+
+    semaphoresList->first = NULL;
+    semaphoresList->iterator = NULL;
+    semaphoresList->last = NULL;
+    semaphoresList->semQty = 0;
+
+    return 0;
+}
+
 semType * openSemaphore(uint32_t id) {
+
+    if(semaphoresList == NULL) {
+        initSemaphores();
+    }
+
     semType * semaphore = findSemaphore(id);
 
     if(semaphore == NULL) {
@@ -10,14 +30,22 @@ semType * openSemaphore(uint32_t id) {
     }
 }
 
-static void createNewSemaphore(uint32_t id. semType * semaphore) {
+static void createNewSemaphore(uint32_t id, semType * semaphore) {
 
     semaphore = mallocMemory(sizeof(semaphore));
 
     semaphore->id;
     semaphore->value;
     semaphore->next = NULL;
+    semaphore->mutex = 0;
 
+    if(semaphoresList->first == NULL) {
+        semaphoresList->first = semaphore;
+        semaphoresList->last = semaphore;
+        semaphoresList->iterator = semaphore;
+    }
+
+    semaphoresList->semQty++;
     semaphoresList->last->next = semaphore;
     semaphoresList->last = semaphore;
 
@@ -25,7 +53,29 @@ static void createNewSemaphore(uint32_t id. semType * semaphore) {
 }
 
 int waitSemphore(uint32_t id) {
-    blockProcess(id);
+    
+    semType * sem = findSemaphore(id);
+    if (sem == NULL) {
+        return -1;
+    }
+
+    acquire(&(sem->mutex));
+
+    if (sem->value > 0)
+    {
+        sem->value--;
+        release(&(sem->mutex));
+        return 0;
+    }
+    else
+    {
+        int currPid = getCurrPID();
+        sem->blockedPIDs[sem->blockedPIDsSize++] = currPid;
+        release(&(sem->mutex));
+        blockProcess(currPid);
+    }
+
+    return 0;
 }
 
 int postSemaphore(uint32_t id) {
@@ -33,7 +83,10 @@ int postSemaphore(uint32_t id) {
 }
 
 void closeSemaphore(uint32_t id) {
-
+    semType * semaphore = findSemaphore(uint32_t id);
+    if(semaphore != NULL) {
+        
+    }
 }
 
 void printSemaphore() {
@@ -50,4 +103,12 @@ static semType * findSemaphore(uint32_t id) {
     }
 
     return NULL;
+}
+
+static void acquire(int *lock) {
+    while (_xchg(lock, 1) != 0);
+}
+
+static void release(int *lock) {
+    _xchg(lock, 0);
 }
