185ee5bc220d36d39c5efc98f0f7a38f6d0b64ff
Juan <jdeluca@itba.edu.ar>
2021-10-31 16:45:30 -0300

    better org

--- Userland/SampleCodeModule/tests/test_mm.c
+++ Userland/SampleCodeModule/tests/test_mm.c
@@ -1,8 +1,8 @@
 #include <test_mm.h>
 
 #define NULL (void *)0
-#define MAX_BLOCKS 12
-#define MAX_MEMORY 1024*1024 //Should be around 80% of memory managed by the MM
+#define MAX_BLOCKS 128
+#define MAX_MEMORY 1024*1024*32 //Should be around 80% of memory managed by the MM
 
 typedef struct MM_rq
 {
@@ -12,67 +12,67 @@ typedef struct MM_rq
 
 void test_mm(int argc, char **argv)
 {
-  void * test = myMalloc(10);
-  if(test == NULL)
-    printf("todo mal 1\n");
-  else {
-    memoryset(test,1,10);
-    if(memcheck(test,1,10) == 0)
-      printf("todo mal 2\n");
-    else
-      {
-        myFree(test);
-        printf("hizo free\n");
-        test = myMalloc(10);
-        memoryset(test,2,10);
-        if (memcheck(test,2,10) == 0)
-          printf("todo mal 3\n");
-        else
-          printf("anduvo 2\n");
-      }
-  }
-  // mm_rq mm_rqs[MAX_BLOCKS];
-
-  // printf("Comienza el test de memoria:\n");
-  // while(1)
-  // {
-  //   uint8_t rq = 0;
-  //   uint32_t total = 0;
-  
-  //   // Request as many blocks as we can
-  //   while (rq < MAX_BLOCKS && total < MAX_MEMORY)
-  //   {
-  //     mm_rqs[rq].size = GetUniform(MAX_MEMORY - total - 1) + 1;
-
-  //     mm_rqs[rq].address = myMalloc(mm_rqs[rq].size);
-  //     if (mm_rqs[rq].address == NULL)
+  // void * test = myMalloc(10);
+  // if(test == NULL)
+  //   printf("todo mal 1\n");
+  // else {
+  //   memoryset(test,1,10);
+  //   if(memcheck(test,1,10) == 0)
+  //     printf("todo mal 2\n");
+  //   else
   //     {
-  //       printf("No memory!\n");
-  //       return;
+  //       myFree(test);
+  //       printf("hizo free\n");
+  //       test = myMalloc(10);
+  //       memoryset(test,2,10);
+  //       if (memcheck(test,2,10) == 0)
+  //         printf("todo mal 3\n");
+  //       else
+  //         printf("anduvo 2\n");
   //     }
-  //     total += mm_rqs[rq].size;
-  //     rq++;
-  //   }
-
-    
-  //   // Set
-  //   uint32_t i;
-  //   for (i = 0; i < rq; i++)
-  //     if (mm_rqs[i].address != NULL)
-  //       memoryset(mm_rqs[i].address, i, mm_rqs[i].size);
-
-    
-  //   // Check
-  //   for (i = 0; i < rq; i++)
-  //     if (mm_rqs[i].address != NULL)
-  //       if (!memcheck(mm_rqs[i].address, i, mm_rqs[i].size))
-  //         printf("error");
-    
-  //   // Free
-  //   for (i = 0; i < rq; i++)
-  //     if (mm_rqs[i].address != NULL)
-  //       myFree(mm_rqs[i].address);
   // }
+  mm_rq mm_rqs[MAX_BLOCKS];
+
+  printf("Comienza el test de memoria:\n");
+  while(1)
+  {
+    uint8_t rq = 0;
+    uint32_t total = 0;
+  
+    // Request as many blocks as we can
+    while (rq < MAX_BLOCKS && total < MAX_MEMORY)
+    {
+      mm_rqs[rq].size = GetUniform(MAX_MEMORY - total - 1) + 1;
+
+      mm_rqs[rq].address = myMalloc(mm_rqs[rq].size);
+      if (mm_rqs[rq].address == NULL)
+      {
+        printf("No memory!\n");
+        return;
+      }
+      total += mm_rqs[rq].size;
+      rq++;
+    }
+
+    
+    // Set
+    uint32_t i;
+    for (i = 0; i < rq; i++)
+      if (mm_rqs[i].address != NULL)
+        memoryset(mm_rqs[i].address, i, mm_rqs[i].size);
+
+    
+    // Check
+    for (i = 0; i < rq; i++)
+      if (mm_rqs[i].address != NULL)
+        if (!memcheck(mm_rqs[i].address, i, mm_rqs[i].size))
+          printf("error");
+    
+    // Free
+    for (i = 0; i < rq; i++)
+      if (mm_rqs[i].address != NULL)
+        myFree(mm_rqs[i].address);
+  }
   
 }
 
